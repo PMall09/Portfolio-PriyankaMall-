ae7d526fa07af15a2921d284e1d5470e22f9f193
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-06 12:43:09 +0530

    Update scripts.js

--- BUILD/scripts.js
+++ BUILD/scripts.js
@@ -1,1 +1,8 @@
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
 
+const jsPath = '_scripts/*.js';
+const destPath = '_site/js';
+
+module.exports = gulp => {
+  gulp.task('scripts', () => {
