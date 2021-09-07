920075470b10729aa790feac399bb49cb08be192
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-07 19:51:34 +0530

    Update sass.js

--- BUILD/sass.js
+++ BUILD/sass.js
@@ -1,3 +1,5 @@
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const scssPath = '_scss/*.scss';
+const destPath = '_site/css';
