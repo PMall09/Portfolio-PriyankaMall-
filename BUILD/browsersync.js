7197af41af2afe4436b584f24e17b98d67dade9d
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-07 19:56:25 +0530

    Update browsersync.js

--- BUILD/browsersync.js
+++ BUILD/browsersync.js
@@ -1,3 +1,12 @@
 const browserSync = require('browser-sync').create();
 const cp = require('child_process');
 const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+const scssPath = '_scss/**/*.scss';
+const jsPath = '_scripts/*.js';
+const templatePath = [
+  '*.html',
+  '+(_includes|_layouts)/*.html',
+  '*.yml',
+  '_data/*.yml',
+  '_posts/*',
+];
