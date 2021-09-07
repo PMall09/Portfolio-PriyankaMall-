85695e06ac56986bf213a2bf4ec7ffd747466c49
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-07 19:52:10 +0530

    Update sass.js

--- BUILD/sass.js
+++ BUILD/sass.js
@@ -5,3 +5,7 @@ const scssPath = '_scss/*.scss';
 const destPath = '_site/css';
 module.exports = gulp => {
   gulp.task('sass', () => {
+    return gulp
+      .src(scssPath)
+      .pipe(
+        sass({
